a801ecfffe4131cdbe01b40be83660b7ce091d07
vitaly <tifoha@gmail.com>
2015-02-24 11:36:11 +0200

    Start

---
+++ src/main/java/com/imcode/entities/AppTableKey.java
@@ -0,0 +1,13 @@
+package com.imcode.entities;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import java.io.Serializable;
+
+/**
+ * Created by vitaly on 20.02.15.
+ */
+//@Embeddable
+public class AppTableKey {
+//    private
+}
---
+++ src/main/java/com/imcode/entities/AppTablePermission.java
@@ -0,0 +1,13 @@
+package com.imcode.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by vitaly on 20.02.15.
+ */
+//@Entity
+//@Table("dbo_appTablesPermissions")
+public class AppTablePermission {
+
+}
