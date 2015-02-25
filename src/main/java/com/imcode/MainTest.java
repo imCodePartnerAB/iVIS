f92dcdaf4e481bc7b9fe4cb37ae65e3fae25c086
vitaly <tifoha@gmail.com>
2015-02-25 12:03:54 +0200

    applicationPermissions

--- src/main/java/com/imcode/MainTest.java
+++ src/main/java/com/imcode/MainTest.java
@@ -1,10 +1,18 @@
 package com.imcode;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 /**
  * Created by vitaly on 17.02.15.
  */
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+//        System.out.println("Hello!");
+//        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+//        ctx.load("src/main/webapp/WEB-INF/spring/app-context-xml.xml");
+//        ctx.refresh();
+
+
     }
 }
--- src/main/java/com/imcode/controllers/AbstractController.java
+++ src/main/java/com/imcode/controllers/AbstractController.java
@@ -1,7 +1,10 @@
 package com.imcode.controllers;
 
+import com.imcode.entities.Role;
 import com.imcode.services.GenericService;
+import com.imcode.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
@@ -15,6 +18,9 @@ public abstract class AbstractController<T, ID extends Serializable, SERVICE_TYP
     @Autowired
     private SERVICE_TYPE service;
 
+    @Autowired
+    private ApplicationContext ctx;
+
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     public @ResponseBody T get(@PathVariable("id") ID id) {
         return service.find(id);
@@ -23,6 +29,8 @@ public abstract class AbstractController<T, ID extends Serializable, SERVICE_TYP
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     public @ResponseBody List<T> getAll() {
         List<T> result = service.findAll();
+//        RoleService roleS = ctx.getBean("RoleService", RoleService.class);
+//        List<Role> roles = roleS.findAll();
         return result;
     }
 
@@ -42,5 +50,13 @@ public abstract class AbstractController<T, ID extends Serializable, SERVICE_TYP
 
     public void setService(SERVICE_TYPE service) {
         this.service = service;
+    }
+
+    public ApplicationContext getCtx() {
+        return ctx;
+    }
+
+    public void setCtx(ApplicationContext ctx) {
+        this.ctx = ctx;
     }
 }
