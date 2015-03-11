797677b7c9a73a55352c2b9474df631158194712
vitaly <tifoha@gmail.com>
2015-03-11 12:14:22 +0200

    OAuth Test

--- src/main/java/com/imcode/controllers/SecurityControllerImpl.java
+++ src/main/java/com/imcode/controllers/SecurityControllerImpl.java
@@ -27,109 +27,109 @@ import java.util.UUID;
 /**
  * Created by vitaly on 25.02.15.
  */
-@Controller
-@DependsOn("securityService")
+//@Controller
+//@DependsOn("securityService")
 public class SecurityControllerImpl {
-    @Autowired
-    private SecurityService securityService;
-
-    @Autowired
-    private ErrorFactory errorFactory;
-
-    @RequestMapping("/autorisation")
-    @ResponseBody
-    Object getAccessToken(@RequestParam("login") String login, 
-                          @RequestParam("pwd") String pwd, 
-                          @RequestParam("app") long appId, WebRequest webRequest, Model model) {
-        UUID uuid = null;
-        try {
-            uuid = securityService.createToken(login, pwd, appId);
-        } catch (ApplicationNotFoundException e) {
-            com.imcode.misc.errors.Error error = errorFactory.getErrorWithDescription(101);
-//            error.setrequestParams(this.);
-            return new ErrorResponse(error);
-        } catch (UserNotfoundException e) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(5));
-        } catch (AutorizationException e) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(5));
-        } catch (RestException e) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(1));
-        }
-
-        TokenInfo tokenInfo = securityService.getTokenInfo(uuid);
-        
-        Map<String, Object> result = new HashMap<>();
-        result.put("access_token", uuid);
-        result.put("refresh_token", tokenInfo.getRefreshToken());
-        result.put("lifetime", tokenInfo.getLifeTime());
-
-        return new NormalResponse(result);
-    }
-
-    @RequestMapping(value = "/autorisation/info", params = "access_token")
-    @ResponseBody
-    Object getTokenInfo(@RequestParam("access_token") String tokenName) {
-        TokenInfo tokenInfo = securityService.getTokenInfo(tokenName);
-        if (tokenInfo == null) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(30));
-        }
-        
-        return new NormalResponse(tokenInfo);
-    }
-
-    @RequestMapping(value = "/autorisation/refresh")
-    @ResponseBody
-    Object refreshToken(@RequestParam("access_token") String tokenName, 
-                        @RequestParam("refresh_token") String refreshTokenName, WebRequest webRequest) {
-        TokenInfo tokenInfo = securityService.getTokenInfo(tokenName);
-        
-        if (tokenInfo == null) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(30));
-        }
-        try {
-            UUID refreshToken = UUID.fromString(refreshTokenName);
-        } catch (Exception e) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(31));
-        }
-        
-        if (!tokenInfo.getRefreshToken().equals(UUID.fromString(refreshTokenName))) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(31));
-        }
-
-        User user = tokenInfo.getUser();
-        UUID newToken = null;
-        
-        try {
-            newToken = securityService.createToken(user.getLogin(), user.getPwd(), tokenInfo.getApplication().getId());
-            securityService.removeToken(tokenInfo.getId());
-            tokenInfo = securityService.getTokenInfo(newToken);
-
-            Map<String, Object> result = new HashMap<>();
-            result.put("access_token", tokenInfo.getId());
-            result.put("refresh_token", tokenInfo.getRefreshToken());
-            result.put("lifetime", tokenInfo.getLifeTime());
-
-            return new NormalResponse(result);
-        } catch (RestException e) {
-            return new ErrorResponse(errorFactory.getErrorWithDescription(1)); 
-        }
-
-    }
-    //Getters & Setters
-    //------------------------------------------------------------------------------------------------------------------
-    public void setSecurityService(SecurityService securityService) {
-        this.securityService = securityService;
-    }
-
-    public SecurityService getSecurityService() {
-        return securityService;
-    }
-
-    public ErrorFactory getErrorFactory() {
-        return errorFactory;
-    }
-
-    public void setErrorFactory(ErrorFactory errorFactory) {
-        this.errorFactory = errorFactory;
-    }
+//    @Autowired
+//    private SecurityService securityService;
+//
+//    @Autowired
+//    private ErrorFactory errorFactory;
+//
+//    @RequestMapping("/autorisation")
+//    @ResponseBody
+//    Object getAccessToken(@RequestParam("login") String login,
+//                          @RequestParam("pwd") String pwd,
+//                          @RequestParam("app") long appId, WebRequest webRequest, Model model) {
+//        UUID uuid = null;
+//        try {
+//            uuid = securityService.createToken(login, pwd, appId);
+//        } catch (ApplicationNotFoundException e) {
+//            com.imcode.misc.errors.Error error = errorFactory.getErrorWithDescription(101);
+////            error.setrequestParams(this.);
+//            return new ErrorResponse(error);
+//        } catch (UserNotfoundException e) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(5));
+//        } catch (AutorizationException e) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(5));
+//        } catch (RestException e) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(1));
+//        }
+//
+//        TokenInfo tokenInfo = securityService.getTokenInfo(uuid);
+//
+//        Map<String, Object> result = new HashMap<>();
+//        result.put("access_token", uuid);
+//        result.put("refresh_token", tokenInfo.getRefreshToken());
+//        result.put("lifetime", tokenInfo.getLifeTime());
+//
+//        return new NormalResponse(result);
+//    }
+//
+//    @RequestMapping(value = "/autorisation/info", params = "access_token")
+//    @ResponseBody
+//    Object getTokenInfo(@RequestParam("access_token") String tokenName) {
+//        TokenInfo tokenInfo = securityService.getTokenInfo(tokenName);
+//        if (tokenInfo == null) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(30));
+//        }
+//
+//        return new NormalResponse(tokenInfo);
+//    }
+//
+//    @RequestMapping(value = "/autorisation/refresh")
+//    @ResponseBody
+//    Object refreshToken(@RequestParam("access_token") String tokenName,
+//                        @RequestParam("refresh_token") String refreshTokenName, WebRequest webRequest) {
+//        TokenInfo tokenInfo = securityService.getTokenInfo(tokenName);
+//
+//        if (tokenInfo == null) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(30));
+//        }
+//        try {
+//            UUID refreshToken = UUID.fromString(refreshTokenName);
+//        } catch (Exception e) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(31));
+//        }
+//
+//        if (!tokenInfo.getRefreshToken().equals(UUID.fromString(refreshTokenName))) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(31));
+//        }
+//
+//        User user = tokenInfo.getUser();
+//        UUID newToken = null;
+//
+//        try {
+//            newToken = securityService.createToken(user.getLogin(), user.getPwd(), tokenInfo.getApplication().getId());
+//            securityService.removeToken(tokenInfo.getId());
+//            tokenInfo = securityService.getTokenInfo(newToken);
+//
+//            Map<String, Object> result = new HashMap<>();
+//            result.put("access_token", tokenInfo.getId());
+//            result.put("refresh_token", tokenInfo.getRefreshToken());
+//            result.put("lifetime", tokenInfo.getLifeTime());
+//
+//            return new NormalResponse(result);
+//        } catch (RestException e) {
+//            return new ErrorResponse(errorFactory.getErrorWithDescription(1));
+//        }
+//
+//    }
+//    //Getters & Setters
+//    //------------------------------------------------------------------------------------------------------------------
+//    public void setSecurityService(SecurityService securityService) {
+//        this.securityService = securityService;
+//    }
+//
+//    public SecurityService getSecurityService() {
+//        return securityService;
+//    }
+//
+//    public ErrorFactory getErrorFactory() {
+//        return errorFactory;
+//    }
+//
+//    public void setErrorFactory(ErrorFactory errorFactory) {
+//        this.errorFactory = errorFactory;
+//    }
 }
--- src/main/java/com/imcode/oauth2/TokenCheckAdvice.java
+++ src/main/java/com/imcode/oauth2/TokenCheckAdvice.java
@@ -16,63 +16,63 @@ import java.util.Map;
  * Created by vitaly on 27.02.15.
  */
 
-@Component
+//@Component
 //@Aspect
 public class TokenCheckAdvice {
-    private SecurityService securityService;
-    @Pointcut("within(@org.springframework.stereotype.Controller *)")
-    public void controllerBean() {}
+//    private SecurityService securityService;
+//    @Pointcut("within(@org.springframework.stereotype.Controller *)")
+//    public void controllerBean() {}
+////
+////    @Pointcut("execution(* *(..))")
+////    public void methodPointcut() {}
+////
+////    @Around("controllerBean() && methodPointcut() ")
+////    @Around("methodPointcut()")
+////@Around("@annotation(org.springframework.stereotype.Controller)")
+////@Around("bean(securityService)")
+////    && @annotation(org.springframework.web.bind.annotation.RequestMapping)
+//    @Around("execution(* com.imcode.controllers..get*(..))")
+//    public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {//, WebRequest webRequest) {
+//        Object retVal = null;
+////        try {
 //
-//    @Pointcut("execution(* *(..))")
-//    public void methodPointcut() {}
+//            Object[] args = pjp.getArgs();
+//            WebRequest webRequest = null;
+//            for (Object arg : args) {
+//                if (arg instanceof WebRequest) {
+//                    webRequest = (WebRequest) arg;
+//                }
+//            }
 //
-//    @Around("controllerBean() && methodPointcut() ")
-//    @Around("methodPointcut()")
-//@Around("@annotation(org.springframework.stereotype.Controller)")
-//@Around("bean(securityService)")
-//    && @annotation(org.springframework.web.bind.annotation.RequestMapping)
-    @Around("execution(* com.imcode.controllers..get*(..))")
-    public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {//, WebRequest webRequest) {
-        Object retVal = null;
-//        try {
-
-            Object[] args = pjp.getArgs();
-            WebRequest webRequest = null;
-            for (Object arg : args) {
-                if (arg instanceof WebRequest) {
-                    webRequest = (WebRequest) arg;
-                }
-            }
-
-            if (webRequest == null) {
-                return pjp.proceed();
-            }
-
-            String accessTokenName = webRequest.getParameter("access_token");
-
-            if (accessTokenName == null) {
-                throw new AutorizationException();
-            }else if (securityService.getTokenInfo(accessTokenName) == null) {
-                throw new AutorizationException();
-            }
-
-            retVal = pjp.proceed();
-//        } catch (Throwable throwable) {
-//            throwable.printStackTrace();
-//        }
-
-        return new NormalResponse(retVal);
-
-//        return retVal;
-        
-    }
-    
-
-    public SecurityService getSecurityService() {
-        return securityService;
-    }
-
-    public void setSecurityService(SecurityService securityService) {
-        this.securityService = securityService;
-    }
+//            if (webRequest == null) {
+//                return pjp.proceed();
+//            }
+//
+//            String accessTokenName = webRequest.getParameter("access_token");
+//
+//            if (accessTokenName == null) {
+//                throw new AutorizationException();
+//            }else if (securityService.getTokenInfo(accessTokenName) == null) {
+//                throw new AutorizationException();
+//            }
+//
+//            retVal = pjp.proceed();
+////        } catch (Throwable throwable) {
+////            throwable.printStackTrace();
+////        }
+//
+//        return new NormalResponse(retVal);
+//
+////        return retVal;
+//
+//    }
+//
+//
+//    public SecurityService getSecurityService() {
+//        return securityService;
+//    }
+//
+//    public void setSecurityService(SecurityService securityService) {
+//        this.securityService = securityService;
+//    }
 }
