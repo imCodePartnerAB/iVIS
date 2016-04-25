b34a48b6c2b88d8fb89bb7039b51e14254b85682
vitaly <tifoha@gmail.com>
2016-04-25 14:38:55 +0300

    fixed bugs

--- ivis-client/src/main/java/com/imcode/imcms/addon/ivisclient/controllers/IvisController.java
+++ ivis-client/src/main/java/com/imcode/imcms/addon/ivisclient/controllers/IvisController.java
@@ -255,7 +255,7 @@ public class IvisController {
                  ApplicationService service = ivisServiceFactory.getService(ApplicationService.class);
                  Application application = service.find(applicationId);
                  Decision decision = application.getDecision();
-                 decision.setComment(decision.getComment() + "<br/>" + commentTextarea);
+                 decision.setComment(!decision.getComment().isEmpty() ? decision.getComment() + "<br/>" + commentTextarea : commentTextarea);
                  application.setDecision(decision);
                  service.save(application);
              }
