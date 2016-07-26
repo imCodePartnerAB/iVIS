767dcd5cac20851a25cafa7279f849c57543aee4
RuslanPopenko <ruslanpopenko@ukr.net>
2016-07-26 13:13:14 +0300

    fixed bugs of checking

--- ivis-server/src/main/webapp/WEB-INF/web-resources/js/registration_validation.js
+++ ivis-server/src/main/webapp/WEB-INF/web-resources/js/registration_validation.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
         //name attribute watch
         $.validator.addMethod("checkUnique", function (value, element, param) {
 
+            if (element.className.indexOf("error") != -1) {
+                return true;
+            }
+
             var flag = false;
             var propertyName = element.getAttribute("name");
             var data = {};
