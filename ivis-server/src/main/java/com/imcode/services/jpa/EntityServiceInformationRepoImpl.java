50fa69f40ff75176e8bb5e38b6caaf87bbacf6e7
RuslanPopenko <ruslanpopenko@ukr.net>
2016-08-01 15:07:43 +0300

    fixed service logic

--- ivis-server/src/main/java/com/imcode/services/jpa/EntityServiceInformationRepoImpl.java
+++ ivis-server/src/main/java/com/imcode/services/jpa/EntityServiceInformationRepoImpl.java
@@ -3,11 +3,13 @@ package com.imcode.services.jpa;
 import com.imcode.entities.EntityServiceInformation;
 import com.imcode.repositories.EntityServiceInformationRepository;
 import com.imcode.services.AbstractService;
+import com.imcode.services.EntityServiceInformationService;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by ruslan on 01.08.16.
  */
 @Service
-public class EntityServiceInformationRepoImpl extends AbstractService<EntityServiceInformation, Long, EntityServiceInformationRepository> {
+public class EntityServiceInformationRepoImpl extends AbstractService<EntityServiceInformation, Long, EntityServiceInformationRepository>
+implements EntityServiceInformationService{
 }
--- ivis-server/src/main/java/com/imcode/services/jpa/ServiceMethodRepoImpl.java
+++ ivis-server/src/main/java/com/imcode/services/jpa/ServiceMethodRepoImpl.java
@@ -3,11 +3,12 @@ package com.imcode.services.jpa;
 import com.imcode.entities.ServiceMethod;
 import com.imcode.repositories.ServiceMethodRepository;
 import com.imcode.services.AbstractService;
+import com.imcode.services.ServiceMethodService;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by ruslan on 01.08.16.
  */
 @Service
-public class ServiceMethodRepoImpl extends AbstractService<ServiceMethod, Long, ServiceMethodRepository> {
+public class ServiceMethodRepoImpl extends AbstractService<ServiceMethod, Long, ServiceMethodRepository> implements ServiceMethodService{
 }
