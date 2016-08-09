90cea41e1c9a996ea1a9674debfc721d9c74697f
RuslanPopenko <ruslanpopenko@ukr.net>
2016-08-09 17:06:20 +0300

    tests on

--- ivis-server/src/test/java/incidentAPI/AbstractTest.java
+++ ivis-server/src/test/java/incidentAPI/AbstractTest.java
@@ -14,59 +14,59 @@ import util.RequestUtil;
  * Created by ruslan on 10.06.16.
  */
 public abstract class AbstractTest {
-//    protected static RequestUtil requestUtil;
-//    protected static int savedId;
-//
-//    @BeforeClass
-//    public static void receiveAccessToken() {
-//        requestUtil = new RequestUtil("http://localhost:8080");
-//        requestUtil.receiveAccessToken();
-//    }
-//
-//    @Rule
-//    public ExpectedException thrown = ExpectedException.none();
-//
-//    @Test
-//    public void testGetAll() throws Exception {
-//
-//        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL()), null, "GET");
-//
-//        JSONArray jsonArray = new JSONArray(json);
-//
-//        assertNotNull(jsonArray);
-//
-//    }
-//
-//    abstract String getAllURL();
-//
-//    @Test
-//    public void testGet() throws Exception {
-//
-//        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() +  additional()), null, "GET");
-//
-//        JSONObject jsonObject = new JSONObject(json);
-//
-//        assertNotNull(jsonObject);
-//
-//        assertEquals(jsonObject.getInt("id"), RequestUtil.getNumberFromStr(additional()));
-//
-//    }
-//
-//    abstract String additional();
-//
-//    abstract void testSave() throws Exception;
-//
-//    abstract void testUpdate() throws Exception;
-//
-//    @Test
-//    public void testDelete() throws Exception {
-//
-//        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() +  "/" + savedId), null, "DELETE");
-//
-//        JSONObject jsonObject = new JSONObject(json);
-//
-//        assertEquals(jsonObject.getInt("id"), savedId);
-//
-//    }
+    protected static RequestUtil requestUtil;
+    protected static int savedId;
+
+    @BeforeClass
+    public static void receiveAccessToken() {
+        requestUtil = new RequestUtil("http://localhost:8080");
+        requestUtil.receiveAccessToken();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testGetAll() throws Exception {
+
+        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL()), null, "GET");
+
+        JSONArray jsonArray = new JSONArray(json);
+
+        assertNotNull(jsonArray);
+
+    }
+
+    abstract String getAllURL();
+
+    @Test
+    public void testGet() throws Exception {
+
+        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() +  additional()), null, "GET");
+
+        JSONObject jsonObject = new JSONObject(json);
+
+        assertNotNull(jsonObject);
+
+        assertEquals(jsonObject.getInt("id"), RequestUtil.getNumberFromStr(additional()));
+
+    }
+
+    abstract String additional();
+
+    abstract void testSave() throws Exception;
+
+    abstract void testUpdate() throws Exception;
+
+    @Test
+    public void testDelete() throws Exception {
+
+        String json = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() +  "/" + savedId), null, "DELETE");
+
+        JSONObject jsonObject = new JSONObject(json);
+
+        assertEquals(jsonObject.getInt("id"), savedId);
+
+    }
 
 }
--- ivis-server/src/test/java/incidentAPI/IncidentTest.java
+++ ivis-server/src/test/java/incidentAPI/IncidentTest.java
@@ -12,86 +12,86 @@ import static org.junit.Assert.*;
  */
 public class IncidentTest extends AbstractTest {
 
-//    @Override
-//    String getAllURL() {
-//        return "incidents";
-//    }
-//
-//    @Override
-//    String additional() {
-//        return "/1";
-//    }
-//
-//    @Test
-//    @Override
-//    public void testSave() throws Exception {
-//        String title = "incident test title";
-//        String description = "incident test description";
-//        int cat1Id = 1;
-//        int cat2Id = 3;
-//        int priorId = 1;
-//        int pup1Id = 10;
-//        int pup2Id = 11;
-//
-//        JSONObject incident = new JSONObject();
-//
-//        incident.put("title", title);
-//
-//        incident.put("description", "incident test description");
-//
-//        JSONArray categories = new JSONArray();
-//        categories.put(new JSONObject().put("id", cat1Id));
-//        categories.put(new JSONObject().put("id", cat2Id));
-//
-//        incident.put("categories", categories);
-//
-//        incident.put("priority", new JSONObject().put("id", priorId));
-//
-//        JSONArray pupils = new JSONArray();
-//        pupils.put(new JSONObject().put("id", pup1Id));
-//        pupils.put(new JSONObject().put("id", pup2Id));
-//
-//        incident.put("pupils", pupils);
-//
-//        String incidentJson = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL()), incident, "POST");
-//
-//        JSONObject incidentJSONObject = new JSONObject(incidentJson);
-//
-//        savedId = incidentJSONObject.getInt("id");
-//
-//        assertEquals(incidentJSONObject.getString("title"), title);
-//        assertEquals(incidentJSONObject.getString("description"), description);
-//
-//        JSONArray categoryResponse = incidentJSONObject.getJSONArray("categories");
-//        JSONObject priorityResponse = incidentJSONObject.getJSONObject("priority");
-//        JSONArray pupilsResponse = incidentJSONObject.getJSONArray("pupils");
-//
-//        assertEquals(categoryResponse.getJSONObject(0).getInt("id"), cat1Id);
-//        assertEquals(categoryResponse.getJSONObject(1).getInt("id"), cat2Id);
-//
-//        assertEquals(priorityResponse.getInt("id"), priorId);
-//
-//        assertEquals(pupilsResponse.getJSONObject(0).getInt("id"), pup1Id);
-//        assertEquals(pupilsResponse.getJSONObject(1).getInt("id"), pup2Id);
-//
-//    }
-//
-//    @Test
-//    @Override
-//    public void testUpdate() throws Exception {
-//        String title = "incident test title changed";
-//
-//        JSONObject incident = new JSONObject();
-//
-//        incident.put("title", title);
-//
-//        String incidentJson = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() + "/" + savedId), incident, "PUT");
-//
-//        JSONObject incidentJSONObject = new JSONObject(incidentJson);
-//
-//        assertEquals(incidentJSONObject.getString("title"), title);
-//
-//    }
+    @Override
+    String getAllURL() {
+        return "incidents";
+    }
+
+    @Override
+    String additional() {
+        return "/1";
+    }
+
+    @Test
+    @Override
+    public void testSave() throws Exception {
+        String title = "incident test title";
+        String description = "incident test description";
+        int cat1Id = 1;
+        int cat2Id = 3;
+        int priorId = 1;
+        int pup1Id = 10;
+        int pup2Id = 11;
+
+        JSONObject incident = new JSONObject();
+
+        incident.put("title", title);
+
+        incident.put("description", "incident test description");
+
+        JSONArray categories = new JSONArray();
+        categories.put(new JSONObject().put("id", cat1Id));
+        categories.put(new JSONObject().put("id", cat2Id));
+
+        incident.put("categories", categories);
+
+        incident.put("priority", new JSONObject().put("id", priorId));
+
+        JSONArray pupils = new JSONArray();
+        pupils.put(new JSONObject().put("id", pup1Id));
+        pupils.put(new JSONObject().put("id", pup2Id));
+
+        incident.put("pupils", pupils);
+
+        String incidentJson = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL()), incident, "POST");
+
+        JSONObject incidentJSONObject = new JSONObject(incidentJson);
+
+        savedId = incidentJSONObject.getInt("id");
+
+        assertEquals(incidentJSONObject.getString("title"), title);
+        assertEquals(incidentJSONObject.getString("description"), description);
+
+        JSONArray categoryResponse = incidentJSONObject.getJSONArray("categories");
+        JSONObject priorityResponse = incidentJSONObject.getJSONObject("priority");
+        JSONArray pupilsResponse = incidentJSONObject.getJSONArray("pupils");
+
+        assertEquals(categoryResponse.getJSONObject(0).getInt("id"), cat1Id);
+        assertEquals(categoryResponse.getJSONObject(1).getInt("id"), cat2Id);
+
+        assertEquals(priorityResponse.getInt("id"), priorId);
+
+        assertEquals(pupilsResponse.getJSONObject(0).getInt("id"), pup1Id);
+        assertEquals(pupilsResponse.getJSONObject(1).getInt("id"), pup2Id);
+
+    }
+
+    @Test
+    @Override
+    public void testUpdate() throws Exception {
+        String title = "incident test title changed";
+
+        JSONObject incident = new JSONObject();
+
+        incident.put("title", title);
+
+        String incidentJson = requestUtil.makeRequest(RequestUtil.genRelUrl(getAllURL() + "/" + savedId), incident, "PUT");
+
+        JSONObject incidentJSONObject = new JSONObject(incidentJson);
+
+        assertEquals(incidentJSONObject.getString("title"), title);
+
+    }
 
 
 
