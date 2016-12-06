0aaca18937629ce5372d3eaadcc0c8cb69273c33
RuslanPopenko <ruslanpopenko@ukr.net>
2016-12-06 10:55:52 +0200

    IVIS-56: Fixed error handling in SDK.

--- ivis-sdk/src/main/java/imcode/services/exceptionhandling/IvisResponseErrorHandler.java
+++ ivis-sdk/src/main/java/imcode/services/exceptionhandling/IvisResponseErrorHandler.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorHandler;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
 
 import java.io.IOException;
 
@@ -28,16 +29,23 @@ public class IvisResponseErrorHandler extends OAuth2ErrorHandler {
     @Override
     public void handleError(ClientHttpResponse response) throws IOException, GeneralException {
 
-        if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
-            ObjectMapper objectMapper = new ObjectMapper();
+        switch (response.getStatusCode()) {
 
-            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+            case BAD_REQUEST:
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+                GeneralException generalException = objectMapper.readValue(response.getBody(), GeneralException.class);
+                throw generalException;
 
-            GeneralException generalException = objectMapper.readValue(response.getBody(), GeneralException.class);
+            case UNAUTHORIZED:
+                throw new UnauthorizedUserException("It is not OAuth2 Authorization.");
 
-            throw generalException;
-        } else {
-            super.handleError(response);
+            case FORBIDDEN:
+                throw new OAuth2AccessDeniedException("Client and/or user has not permission to access API method.");
+
+            default:
+                super.handleError(response);
+
         }
     }
 }
