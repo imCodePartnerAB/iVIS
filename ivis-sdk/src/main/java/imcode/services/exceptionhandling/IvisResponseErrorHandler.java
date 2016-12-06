3046c0648145f37c77f1eddeecf7db1e90414833
RuslanPopenko <ruslanpopenko@ukr.net>
2016-12-06 15:38:34 +0200

    IVIS-56: Fixed IvisResponseErrorHandler that process only general exception.

--- ivis-sdk/src/main/java/imcode/services/exceptionhandling/IvisResponseErrorHandler.java
+++ ivis-sdk/src/main/java/imcode/services/exceptionhandling/IvisResponseErrorHandler.java
@@ -5,11 +5,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorHandler;
-import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by ruslan on 25.08.16.
@@ -32,20 +31,24 @@ public class IvisResponseErrorHandler extends OAuth2ErrorHandler {
         switch (response.getStatusCode()) {
 
             case BAD_REQUEST:
-                ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-                GeneralException generalException = objectMapper.readValue(response.getBody(), GeneralException.class);
-                throw generalException;
+                throw buildException(response.getBody());
 
             case UNAUTHORIZED:
-                throw new UnauthorizedUserException("It is not OAuth2 Authorization.");
+                throw buildException(response.getBody());
 
             case FORBIDDEN:
-                throw new OAuth2AccessDeniedException("Client and/or user has not permission to access API method.");
+                throw buildException(response.getBody());
 
             default:
                 super.handleError(response);
 
         }
     }
-}
+
+    private GeneralException buildException(InputStream responseBody) throws IOException {
+        return new ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
+                .readValue(responseBody, GeneralException.class);
+    }
+
+ }
