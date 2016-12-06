3cf73058fe16bb8604261e2a19b107bc0d9968e9
RuslanPopenko <ruslanpopenko@ukr.net>
2016-12-06 15:20:38 +0200

    IVIS-56: Create IvisAuthorizationCodeResourceDetails.

---
+++ ivis-sdk/src/main/java/imcode/services/oauth2/IvisAuthorizationCodeResourceDetails.java
@@ -0,0 +1,19 @@
+package imcode.services.oauth2;
+
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+/**
+ * Created by vitaly on 02.06.15.
+ */
+public class IvisAuthorizationCodeResourceDetails extends AuthorizationCodeResourceDetails {
+    private boolean clientOnly = true;
+
+    @Override
+    public boolean isClientOnly() {
+        return clientOnly;
+    }
+
+    public void setClientOnly(boolean clientOnly) {
+        this.clientOnly = clientOnly;
+    }
+}
