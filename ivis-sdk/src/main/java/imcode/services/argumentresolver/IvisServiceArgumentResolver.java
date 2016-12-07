0bd67e7d5b03b48e478ee7d20986ce6e1b4292f5
RuslanPopenko <ruslanpopenko@ukr.net>
2016-12-07 11:54:26 +0200

    IVIS-56: Change interface for IvisServiceArgumentResolver.

--- ivis-sdk/src/main/java/imcode/services/argumentresolver/IvisServiceArgumentResolver.java
+++ ivis-sdk/src/main/java/imcode/services/argumentresolver/IvisServiceArgumentResolver.java
@@ -4,29 +4,29 @@ import com.imcode.services.GenericService;
 import imcode.services.IvisServiceFactory;
 import imcode.services.utils.IvisOAuth2Utils;
 import org.springframework.core.MethodParameter;
-import org.springframework.web.bind.support.WebArgumentResolver;
+import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by ruslan on 06.12.16.
  */
-public class IvisServiceArgumentResolver implements WebArgumentResolver {
+public class IvisServiceArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
-    public Object resolveArgument(MethodParameter methodParameter, NativeWebRequest webRequest) throws Exception {
-        Class<?> type = methodParameter.getParameterType();
-        Object nativeRequest = webRequest.getNativeRequest();
-        if (!GenericService.class.isAssignableFrom(type) && !(nativeRequest instanceof HttpServletRequest)) {
-            return UNRESOLVED;
-        }
+    public boolean supportsParameter(MethodParameter parameter) {
+        return GenericService.class.isAssignableFrom(parameter.getParameterType());
+    }
 
-        HttpServletRequest request = (HttpServletRequest) nativeRequest;
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Class<?> type = parameter.getParameterType();
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         IvisServiceFactory ivisServiceFactory = IvisOAuth2Utils.getServiceFactory(request);
 
         return ivisServiceFactory.getService((Class<? extends GenericService>) type);
-
     }
-
 }
