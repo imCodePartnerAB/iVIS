5f67d44245735bebd9e2db5917119f7d9ca28507
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-12 11:16:28 +0300

    IVIS-71 - Connect Next Cloud and iVIS: - Showing error message when Next Cloud access is not allowed.

--- ivis-server/src/main/java/com/imcode/controllers/sso/SsoController.java
+++ ivis-server/src/main/java/com/imcode/controllers/sso/SsoController.java
@@ -1,6 +1,9 @@
 package com.imcode.controllers.sso;
 
+import com.imcode.entities.User;
+import com.imcode.exceptions.SchoolCloudAccessException;
 import com.imcode.services.SsoService;
+import com.imcode.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +16,26 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/idp")
 public class SsoController {
 
+    private static final String ACCESS_ERROR_MESSAGE =
+            "You are not entitled to access iVIS School Cloud – please check with your administrator.";
+
     private final SsoService ssoService;
+    private final UserService userService;
 
     @Autowired
-    public SsoController(SsoService ssoService) {
+    public SsoController(SsoService ssoService, UserService userService) {
         this.ssoService = ssoService;
+        this.userService = userService;
     }
 
     @RequestMapping(value = "/SingleSignOnService", method = RequestMethod.GET)
     public void singleSignOnService(final HttpServletRequest request, final HttpServletResponse response) {
+        final User loggedUser = userService.findByUsername(request.getUserPrincipal().getName());
+
+        if (!loggedUser.getNextCloudEnabled()) {
+            throw new SchoolCloudAccessException(ACCESS_ERROR_MESSAGE);
+        }
+
         ssoService.logIn(request, response);
     }
 
