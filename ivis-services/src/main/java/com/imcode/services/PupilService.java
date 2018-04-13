25c545542ea1e41ade871404a0ea6a027fba753d
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-13 16:52:39 +0300

    IVIS-71 - Connect Next Cloud and iVIS: - Added method to pupil service for finding pupil by person.

--- ivis-services/src/main/java/com/imcode/services/PupilService.java
+++ ivis-services/src/main/java/com/imcode/services/PupilService.java
@@ -1,10 +1,12 @@
 package com.imcode.services;
 
+import com.imcode.entities.Person;
 import com.imcode.entities.Pupil;
 
 /**
  * Created by vitaly on 17.02.15.
  */
 public interface PupilService extends GenericService<Pupil, Long>, PersonalizedService<Pupil> {
-//    Pupil findByPersonalId(String personalId);
+
+    Pupil getPupilByPerson(Person person);
 }
