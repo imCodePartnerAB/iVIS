25ecf05b1c8b8bf2e17e994bce88f75ebc9223d7
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-13 16:50:01 +0300

    IVIS-71 - Connect Next Cloud and iVIS: - Finding pupil by person id.

--- ivis-server/src/main/java/com/imcode/repositories/PupilRepository.java
+++ ivis-server/src/main/java/com/imcode/repositories/PupilRepository.java
@@ -2,11 +2,13 @@ package com.imcode.repositories;
 
 import com.imcode.entities.Pupil;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by vitaly on 13.05.15.
  */
 public interface PupilRepository extends PersonalizedRepository<Pupil>, JpaSpecificationExecutor<Pupil> {
-//    @Query("select p from Pupil p where p.person.personalId = :personalId")
-//    Pupil findByPersonalId(@Param("personalId") String personalId);
+
+    @Query("SELECT pupil FROM Pupil pupil WHERE pupil.person.id = ?1")
+    Pupil findPupilByPersonId(Long personId);
 }
