77bce9ee79a1c39e3784ddc1344a19c8db661a7f
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-16 12:12:16 +0300

    IVIS-71 - Connect Next Cloud and iVIS: - Added method for modifying School Cloud enabling value of school class.

--- ivis-server/src/main/java/com/imcode/repositories/SchoolClassRepository.java
+++ ivis-server/src/main/java/com/imcode/repositories/SchoolClassRepository.java
@@ -5,6 +5,12 @@ import com.imcode.entities.SchoolClass;
 import com.imcode.services.NamedService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long>, NamedService<SchoolClass>, JpaSpecificationExecutor<SchoolClass> {
+
+    @Modifying
+    @Query("UPDATE SchoolClass sc SET sc.nextCloudEnabled = ?2 WHERE sc.id = ?1")
+    void updateSchoolCloudEnabling(Long id, boolean nextCloudEnabled);
 }
